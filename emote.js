cd721ee74819fd136e94c57d523f4cd6841b1be5
Crypto <walterbishoppen@gmail.com>
2020-04-15 19:25:57 +0200

    fix
    fix

--- emote.js
+++ emote.js
@@ -10,31 +10,31 @@ const headers = { 'Content-Type': 'application/json' }
 const Discord = require('discord.js')
 
 
-const discordColors = {
-   DEFAULT: 0,
-   AQUA: 1752220,
-   GREEN: 3066993,
-   BLUE: 3447003,
-   PURPLE: 10181046,
-   GOLD: 15844367,
-   ORANGE: 15105570,
-   RED: 15158332,
-   GREY: 9807270,
-   DARKER_GREY: 8359053,
-   NAVY: 3426654,
-   DARK_AQUA: 1146986,
-   DARK_GREEN: 2067276,
-   DARK_BLUE: 2123412,
-   DARK_PURPLE: 7419530,
-   DARK_GOLD: 12745742,
-   DARK_ORANGE: 11027200,
-   DARK_RED: 10038562,
-   DARK_GREY: 9936031,
-   LIGHT_GREY: 12370112,
-   DARK_NAVY: 2899536,
-   LUMINOUS_VIVID_PINK: 16580705,
-   DARK_VIVID_PINK: 12320855
-}
+const discordColors = [
+   0,
+   1752220,
+   3066993,
+   3447003,
+   10181046,
+   15844367,
+   15105570,
+   15158332,
+   9807270,
+   8359053,
+   3426654,
+   1146986,
+   2067276,
+   2123412,
+   7419530,
+   12745742,
+   11027200,
+   10038562,
+   9936031,
+   12370112,
+   2899536,
+   16580705,
+   12320855
+]
 
 
 class Emote {
@@ -90,7 +90,7 @@ class Emote {
 
 
         let color = discordColors[colorRandomize]
-
+        message.channel.send('current color:' + color)
 
         message.channel.send('The Current Offset is: ' + offsetRandomize)
         let apiURL = "https://api.giphy.com/v1/gifs/search?limit=20&offset=" + offsetRandomize + "&q=" + params + apiKey

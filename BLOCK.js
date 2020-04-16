247ac9aec351a049f42b25e8ce2a7059860e0eb3
Crypto <walterbishoppen@gmail.com>
2020-04-17 00:11:44 +0200

    added fixes i guess

--- BLOCK.js
+++ BLOCK.js
@@ -77,78 +77,82 @@ class BLOCK {
 
                 let color = discordColors[colorRandomize]
 
-                let apiURL = "https://api.giphy.com/v1/gifs/search?limit=20&offset=" + offsetRandomize + "&q=" + params + "+" + "anime" + apiKey
-
-                await fetch(apiURL, { method: "GET", headers: headers })
-                .then((resp) => resp.json())
-                .then((object) => {
-    
-    
-                    //message.channel.send("DEBUG INFO: The limit is 20 and i got " + object['data'].length + " objects.")
-    
-    
-    
-                    let random = Math.floor(Math.random() * 21);
-    
-                    if (object['data'].length < 20) {
-                        random = Math.floor(Math.random() * (object['data'].length))
-                    }
-    
-                    let coderun = false;
-                    while (coderun == false) {
-                        if (object['data'][random]['embed_url'] === undefined) {
-                            console.log("Something was wrong");
-                            random = Math.floor(Math.random() * 21);
-                        } else if (object['data'][random]['images']['original']['url'] != undefined) {
-                            coderun = true;
-                        } else {
-                            coderun = true;
-                            break;
-                        }
-                    }
-    
-                    try {
-                        //str.substring(0, str.length() - 1)
-                        //message.channel.send(message.member.user.tag + " cuddles with: " + args + ": " + "\n" + object['data'][random]['embed_url'])
-    
-                        // using embed function instead
-                        console.log("Image number: " + random)
-                            //const myEmbed = new Discord.RichEmbed()
-                            //    .setColor(15844367)
-                            //    .setTitle(client.user.username + " send cuddles to " + args)
-                            //    .addField(client.user.username + " cuddles with " + args)
-                            //    .setImage(object['data'][random]['embed_url'])
-    
-                        //message.channel.send({ myEmbed });
-                        let url = object['data'][random]['images']['original']['url']
-    
-
-
-                        let BlockEmbed = {
-                            "content": params,
-                            "title": "Block Halving Countdown",
-                            "description": "Time left until 2020 halving: " + daysLeft.toFixed(2) + "days " + hoursLeft.toFixed(2) + "hours " + minutesleft.toFixed(2) + "minutes ",
-                            "url": "https://www.bitcoinblockhalf.com/",
-                            "color": color,
-                            "timestamp": dateTime,
-                            "image": {
-                                "url": url
-                            }
-                        }
-        
-                        //channel.send({ embed: cuddleEmbed });
-                        message.channel.send({ embed: BlockEmbed });
-
-
-
-     
-    
-                    } catch (error) {
-                        message.channel.send(error.message);
-                    }
-                })
+                getImage(color, daysLeft, hoursLeft, minutesleft, blocksLeft, currentBlock, colorRandomize, offsetRandomize)
         })
     }
+
+async getImage(color, daysLeft, hoursLeft, minutesleft, blocksLeft, currentBlock, colorRandomize, offsetRandomize){
+let apiURL = "https://api.giphy.com/v1/gifs/search?limit=20&offset=" + offsetRandomize + "&q=" + params + "+" + "anime" + apiKey
+    await fetch(apiURL, { method: "GET", headers: headers })
+    .then((resp) => resp.json())
+    .then((object) => {
+
+
+        //message.channel.send("DEBUG INFO: The limit is 20 and i got " + object['data'].length + " objects.")
+
+
+
+        let random = Math.floor(Math.random() * 21);
+
+        if (object['data'].length < 20) {
+            random = Math.floor(Math.random() * (object['data'].length))
+        }
+
+        let coderun = false;
+        while (coderun == false) {
+            if (object['data'][random]['embed_url'] === undefined) {
+                console.log("Something was wrong");
+                random = Math.floor(Math.random() * 21);
+            } else if (object['data'][random]['images']['original']['url'] != undefined) {
+                coderun = true;
+            } else {
+                coderun = true;
+                break;
+            }
+        }
+
+        try {
+            //str.substring(0, str.length() - 1)
+            //message.channel.send(message.member.user.tag + " cuddles with: " + args + ": " + "\n" + object['data'][random]['embed_url'])
+
+            // using embed function instead
+            console.log("Image number: " + random)
+                //const myEmbed = new Discord.RichEmbed()
+                //    .setColor(15844367)
+                //    .setTitle(client.user.username + " send cuddles to " + args)
+                //    .addField(client.user.username + " cuddles with " + args)
+                //    .setImage(object['data'][random]['embed_url'])
+
+            //message.channel.send({ myEmbed });
+            let url = object['data'][random]['images']['original']['url']
+
+
+
+            let BlockEmbed = {
+                "content": params,
+                "title": "Block Halving Countdown",
+                "description": "Time left until 2020 halving: " + daysLeft.toFixed(2) + "days " + hoursLeft.toFixed(2) + "hours " + minutesleft.toFixed(2) + "minutes ",
+                "url": "https://www.bitcoinblockhalf.com/",
+                "color": color,
+                "timestamp": dateTime,
+                "image": {
+                    "url": url
+                }
+            }
+
+            //channel.send({ embed: cuddleEmbed });
+            message.channel.send({ embed: BlockEmbed });
+
+
+
+
+
+        } catch (error) {
+            message.channel.send(error.message);
+        }
+    })
+    }
+
 
 }
 module.exports = BLOCK

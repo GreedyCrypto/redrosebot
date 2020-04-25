44417fb32522fba52d688e7dcfc330d2641facd2
GreedyCrypto <walterbishoppen@gmail.com>
2020-04-25 14:14:23 +0200

    removed useless do-while loop

--- vrchat.js
+++ vrchat.js
@@ -141,11 +141,6 @@ class VRC {
 
 
     static async getByUserName(message, args) {
-
-
-
-
-
         async function tagsort(tags) {
             console.log("Mein wert ist " + tags)
                 // Check for null or undefined in IndexOf tags //
@@ -170,162 +165,146 @@ class VRC {
         }
 
 
+        let searchuser = args
+        let apiKey = "&apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26"
+        endpoint = "users" + "?search=" + searchuser
+        try {
+            await fetch(apiURL + endpoint + apiKey, { method: 'GET', headers: headers }, false)
+                .then(response => response.json())
+                .then(async function setData(object) {
+                    console.log(object)
+                    if (object.length == 0) {
+                        message.channel.send("I didn't found any user. Bazinga.")
+                    } else {
+
+                        var today = new Date()
+                        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+                        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+                        var dateTime = date + ' ' + time
 
 
 
-
-
-        let end = false
-        do {
-            let searchuser = args
-            let apiKey = "&apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26"
-            endpoint = "users" + "?search=" + searchuser
-            try {
-                await fetch(apiURL + endpoint + apiKey, { method: 'GET', headers: headers }, false)
-                    .then(response => response.json())
-                    .then(async function setData(object) {
-                        console.log(object)
-                        if (object.length == 0) {
-                            message.channel.send("I didn't found any user. Bazinga.")
-                            end = true
-                        } else {
-
-                            var today = new Date()
-                            var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
-                            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
-                            var dateTime = date + ' ' + time
-
-
-
-                            let colorRandomize = Math.floor(Math.random() * 24)
-                            while (colorRandomize > 23) {
-                                colorRandomize = Math.floor(Math.random() * 24)
-                            }
-
-                            let color = discordColors[colorRandomize]
-
-
-                            if (object[0] == null) {
-                                console.log("im undefined")
-                                end = true
-                                return
-                            }
-
-
-                            let tags = object[0]['tags']
-
-                            let bio = "";
-
-                            let params = "vrchat"
-
-                            if (object[0]['bio'] == "" || object[0]['bio'] == null || object[0]['bio'] == undefined) {
-                                bio = ""
-                            } else {
-                                bio = object[0]['bio']
-                            }
-
-
-                            let vrcEmbed = {
-                                    "content": params,
-                                    "title": object[0]['displayName'],
-                                    "description": bio + " " + " | Rank: " + await tagsort(tags),
-                                    "url": "https://vrchat.com/home/user/" + object[0]['id'],
-                                    "color": color,
-                                    "timestamp": dateTime,
-                                    "image": {
-                                        "url": object[0]['currentAvatarImageUrl']
-                                    },
-                                    "thumbnail": {
-                                        "url": object[0]['currentAvatarThumbnailImageUrl']
-                                    }
-                                }
-                                //channel.send({ embed: cuddleEmbed })
-                            message.channel.send({ embed: vrcEmbed })
-
-                            if (object[1] == null) {
-                                console.log("im undefined")
-                                end = true
-                                return
-                            }
-
-
-                            let tags2 = object[1]['tags']
-
-                            let bio1 = "";
-
-                            let params1 = "vrchat"
-
-                            if (object[1]['bio'] == "" || object[1]['bio'] == null || object[1]['bio'] == undefined) {
-                                bio1 = ""
-                            } else {
-                                bio1 = object[1]['bio']
-                            }
-
-                            let vrcEmbed2 = {
-                                "content": params1,
-                                "title": object[1]['displayName'],
-                                "description": bio1 + " " + " | Rank: " + await tagsort(tags2),
-                                "url": "https://vrchat.com/home/user/" + object[1]['id'],
-                                "color": color,
-                                "timestamp": dateTime,
-                                "image": {
-                                    "url": object[1]['currentAvatarImageUrl']
-                                },
-                                "thumbnail": {
-                                    "url": object[1]['currentAvatarThumbnailImageUrl']
-                                }
-                            }
-
-                            //channel.send({ embed: cuddleEmbed })
-                            message.channel.send({ embed: vrcEmbed2 })
-
-                            if (object[2] == null) {
-                                console.log("im undefined")
-                                end = true
-                                return
-                            }
-
-
-
-                            let tags3 = object[2]['tags']
-
-                            let bio2 = "";
-
-                            let params2 = "vrchat"
-
-                            if (object[2]['bio'] == "" || object[2]['bio'] == null || object[2]['bio'] == undefined) {
-                                bio2 = ""
-                            } else {
-                                bio2 = object[2]['bio']
-                            }
-
-                            let vrcEmbed3 = {
-                                "content": params2,
-                                "title": object[2]['displayName'],
-                                "description": bio2 + " " + " | Rank: " + await tagsort(tags3),
-                                "url": "https://vrchat.com/home/user/" + object[2]['id'],
-                                "color": color,
-                                "timestamp": dateTime,
-                                "image": {
-                                    "url": object[2]['currentAvatarImageUrl']
-                                },
-                                "thumbnail": {
-                                    "url": object[2]['currentAvatarThumbnailImageUrl']
-                                }
-                            }
-
-                            //channel.send({ embed: cuddleEmbed })
-                            message.channel.send({ embed: vrcEmbed3 })
-
-                            end = true
+                        let colorRandomize = Math.floor(Math.random() * 24)
+                        while (colorRandomize > 23) {
+                            colorRandomize = Math.floor(Math.random() * 24)
                         }
-                    })
-            } catch (ex) {
-                message.channel.send(ex.message, "User not found. Please try again.")
-                end = false
-            }
+
+                        let color = discordColors[colorRandomize]
+
+
+                        if (object[0] == null) {
+                            console.log("im undefined")
+                            return
+                        }
+
+
+                        let tags = object[0]['tags']
+
+                        let bio = "";
+
+                        let params = "vrchat"
+
+                        if (object[0]['bio'] == "" || object[0]['bio'] == null || object[0]['bio'] == undefined) {
+                            bio = ""
+                        } else {
+                            bio = object[0]['bio']
+                        }
+
+
+                        let vrcEmbed = {
+                                "content": params,
+                                "title": object[0]['displayName'],
+                                "description": bio + " " + " | Rank: " + await tagsort(tags),
+                                "url": "https://vrchat.com/home/user/" + object[0]['id'],
+                                "color": color,
+                                "timestamp": dateTime,
+                                "image": {
+                                    "url": object[0]['currentAvatarImageUrl']
+                                },
+                                "thumbnail": {
+                                    "url": object[0]['currentAvatarThumbnailImageUrl']
+                                }
+                            }
+                            //channel.send({ embed: cuddleEmbed })
+                        message.channel.send({ embed: vrcEmbed })
+
+                        if (object[1] == null) {
+                            console.log("im undefined")
+                            return
+                        }
+
+
+                        let tags2 = object[1]['tags']
+
+                        let bio1 = "";
+
+                        let params1 = "vrchat"
+
+                        if (object[1]['bio'] == "" || object[1]['bio'] == null || object[1]['bio'] == undefined) {
+                            bio1 = ""
+                        } else {
+                            bio1 = object[1]['bio']
+                        }
+
+                        let vrcEmbed2 = {
+                            "content": params1,
+                            "title": object[1]['displayName'],
+                            "description": bio1 + " " + " | Rank: " + await tagsort(tags2),
+                            "url": "https://vrchat.com/home/user/" + object[1]['id'],
+                            "color": color,
+                            "timestamp": dateTime,
+                            "image": {
+                                "url": object[1]['currentAvatarImageUrl']
+                            },
+                            "thumbnail": {
+                                "url": object[1]['currentAvatarThumbnailImageUrl']
+                            }
+                        }
+
+                        //channel.send({ embed: cuddleEmbed })
+                        message.channel.send({ embed: vrcEmbed2 })
+
+                        if (object[2] == null) {
+                            console.log("im undefined")
+                            return
+                        }
+
+
+
+                        let tags3 = object[2]['tags']
+
+                        let bio2 = "";
+
+                        let params2 = "vrchat"
+
+                        if (object[2]['bio'] == "" || object[2]['bio'] == null || object[2]['bio'] == undefined) {
+                            bio2 = ""
+                        } else {
+                            bio2 = object[2]['bio']
+                        }
+
+                        let vrcEmbed3 = {
+                            "content": params2,
+                            "title": object[2]['displayName'],
+                            "description": bio2 + " " + " | Rank: " + await tagsort(tags3),
+                            "url": "https://vrchat.com/home/user/" + object[2]['id'],
+                            "color": color,
+                            "timestamp": dateTime,
+                            "image": {
+                                "url": object[2]['currentAvatarImageUrl']
+                            },
+                            "thumbnail": {
+                                "url": object[2]['currentAvatarThumbnailImageUrl']
+                            }
+                        }
+
+                        //channel.send({ embed: cuddleEmbed })
+                        message.channel.send({ embed: vrcEmbed3 })
+                    }
+                })
+        } catch (ex) {
+            message.channel.send(ex.message, "User not found. Please try again.")
         }
-        while (end == false)
         console.log("Request Proceeded Successfully.")
     }
 

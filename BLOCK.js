f35d28b1479c6b70709415cfa12edc49e8256e04
GreedyCrypto <walterbishoppen@gmail.com>
2020-05-03 03:10:10 +0200

    testing something

--- BLOCK.js
+++ BLOCK.js
@@ -19,20 +19,20 @@ class BLOCK {
 
 
 
-    static async btc_current_price(client, message, args){
+    static async btc_current_price(client, message, args) {
         let headersCMC = { 'Content-Type': 'application/json' }
         let endpoint = "cryptocurrency/quotes/latest"
         let query = "?id=1"
-        let apiURLCMC = "https://pro-api.coinmarketcap.com/v1/" + endpoint  + query + "&CMC_PRO_API_KEY=" + cmc_apiKey 
-        await fetch(apiURLCMC, {method: "GET", headers: headersCMC})
-        .then((resp) => resp.json())
-        .then((object) => {
-            try{
-                message.reply('Current Bitcoin price: ' + object['data']['1']['quote']['USD']['price'].toFixed(2) + ' USD')
-            }catch(err){
-                message.reply(err.message)
-            }
-        })
+        let apiURLCMC = "https://pro-api.coinmarketcap.com/v1/" + endpoint + query + "&CMC_PRO_API_KEY=" + cmc_apiKey
+        await fetch(apiURLCMC, { method: "GET", headers: headersCMC })
+            .then((resp) => resp.json())
+            .then((object) => {
+                try {
+                    message.reply('Current Bitcoin price: ' + object['data']['1']['quote']['USD']['price'].toFixed(2) + ' USD')
+                } catch (err) {
+                    message.reply(err.message)
+                }
+            })
     }
 
 
@@ -45,10 +45,10 @@ class BLOCK {
         await fetch(APIURL, { method: "GET", headers: headers })
             .then((resp) => resp.json())
             .then((object) => {
-            
+
 
                 var today = new Date()
-                var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+                var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
                 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
                 var dateTime = date + ' ' + time
 
@@ -63,43 +63,43 @@ class BLOCK {
 
                 let offsetRandomize = Math.floor(Math.random() * 10)
 
-                while(offsetRandomize > 5) {
+                while (offsetRandomize > 5) {
                     offsetRandomize = Math.floor(Math.random() * 10)
                 }
-        
-        
+
+
                 let colorRandomize = Math.floor(Math.random() * 24)
-                while(colorRandomize > 23){
-                colorRandomize = Math.floor(Math.random() * 24)
+                while (colorRandomize > 23) {
+                    colorRandomize = Math.floor(Math.random() * 24)
                 }
 
                 let color = discordColors[colorRandomize]
 
-                getImage(color, daysLeft, hoursLeft, minutesleft, blocksLeft, currentBlock, colorRandomize, offsetRandomize, params, dateTime)
-        })
+                getImage(color, daysLeft, hoursLeft, minutesleft, blocksLeft, currentBlock, colorRandomize, offsetRandomize, params, dateTime, args)
+            })
 
 
 
 
 
-        async function getImage(color, daysLeft, hoursLeft, minutesleft, blocksLeft, currentBlock, colorRandomize, offsetRandomize, params, dateTime){
+        async function getImage(color, daysLeft, hoursLeft, minutesleft, blocksLeft, currentBlock, colorRandomize, offsetRandomize, params, dateTime, args) {
             let apiKey = giphy_apiKey
             let apiURL = "https://api.giphy.com/v1/gifs/search?limit=20&offset=" + offsetRandomize + "&q=" + "Buy+Bitcoin" + apiKey
-                await fetch(apiURL, { method: "GET", headers: headers })
+            await fetch(apiURL, { method: "GET", headers: headers })
                 .then((resp) => resp.json())
                 .then((object) => {
-            
-            
+
+
                     //message.channel.send("DEBUG INFO: The limit is 20 and i got " + object['data'].length + " objects.")
-            
-            
-            
+
+
+
                     let random = Math.floor(Math.random() * 21)
-            
+
                     if (object['data'].length < 20) {
                         random = Math.floor(Math.random() * (object['data'].length))
                     }
-            
+
                     let coderun = false
                     while (coderun == false) {
                         if (object['data'][random]['embed_url'] === undefined) {
@@ -112,11 +112,11 @@ class BLOCK {
                             break
                         }
                     }
-            
+
                     try {
                         //str.substring(0, str.length() - 1)
                         //message.channel.send(message.member.user.tag + " cuddles with: " + args + ": " + "\n" + object['data'][random]['embed_url'])
-            
+
                         // using embed function instead
                         console.log("Image number: " + random)
                             //const myEmbed = new Discord.RichEmbed()
@@ -124,12 +124,12 @@ class BLOCK {
                             //    .setTitle(client.user.username + " send cuddles to " + args)
                             //    .addField(client.user.username + " cuddles with " + args)
                             //    .setImage(object['data'][random]['embed_url'])
-            
+
                         //message.channel.send({ myEmbed })
                         let url = object['data'][random]['images']['original']['url']
-            
-            
-            
+
+
+
                         let BlockEmbed = {
                             "content": params,
                             "title": "Block Halving Countdown | Blocks left: " + blocksLeft,
@@ -141,20 +141,23 @@ class BLOCK {
                                 "url": url
                             }
                         }
-            
+
                         //channel.send({ embed: cuddleEmbed })
-                        message.channel.send({ embed: BlockEmbed })
-            
-            
-            
-            
-            
+                        if (args === 'IAMBOT') {
+                            client.channels.cachel.get('698561456354099250').send({ embed: BlockEmbed })
+                        } else {
+                            message.channel.send({ embed: BlockEmbed })
+                        }
+
+
+
+
                     } catch (error) {
                         message.channel.send(error.message)
                     }
                 })
-                }
-            
+        }
+
     }
 
 }
